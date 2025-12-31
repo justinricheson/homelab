157b55fcb1ae1bb81f73305f8395f9eac6294726
Justin Richeson <justin@richeson.com>
2025-12-31 14:44:29 -0600

    Setup tailscale

--- k0s-cluster/install.sh
+++ k0s-cluster/install.sh
@@ -4,14 +4,15 @@ set -e
 
 export KUBECONFIG=~/.kube/config-pi1
 
-VERSION_METALLB=0.15.2        # https://metallb.io                - helm search repo metallb/metallb --versions
-VERSION_CERT_MANAGER=v1.18.2  # https://cert-manager.io           - https://quay.io/repository/jetstack/charts/cert-manager?tab=tags
-VERSION_TRAEFIK=37.0.0        # https://traefik.io                - helm search repo traefik/traefik --versions
-VERSION_LONGHORN=1.9.1        # https://longhorn.io               - helm search repo longhorn --versions
-VERSION_TECHNITIUM=13.6.0     # https://technitium.com/dns        - https://hub.docker.com/r/technitium/dns-server/tags
-VERSION_GO2RTC=1.9.10         # https://github.com/AlexxIT/go2rtc - https://hub.docker.com/r/alexxit/go2rtc/tags
-VERSION_FRIGATE=7.8.0         # https://frigate.video             - helm search repo blakeblackshear/frigate --versions
+VERSION_METALLB_HELM=0.15.2   # https://metallb.io                - helm search repo metallb/metallb --versions
+VERSION_CERT_MGR_HELM=v1.18.2 # https://cert-manager.io           - https://quay.io/repository/jetstack/charts/cert-manager?tab=tags
+VERSION_TRAEFIK_HELM=37.0.0   # https://traefik.io                - helm search repo traefik/traefik --versions
+VERSION_LONGHORN_HELM=1.9.1   # https://longhorn.io               - helm search repo longhorn --versions
+VERSION_TECHNITIUM_IMG=13.6.0 # https://technitium.com/dns        - https://hub.docker.com/r/technitium/dns-server/tags
+VERSION_GO2RTC_IMG=1.9.10     # https://github.com/AlexxIT/go2rtc - https://hub.docker.com/r/alexxit/go2rtc/tags
+VERSION_FRIGATE_HELM=7.8.0    # https://frigate.video             - helm search repo blakeblackshear/frigate --versions
 VERSION_FRIGATE_IMG=0.16.1    # https://frigate.video             - https://github.com/blakeblackshear/frigate/releases
+VERSION_TAILSCALE_IMG=v1.92.4 # https://tailscale.com             - https://hub.docker.com/r/tailscale/tailscale/tags
 
 ./scripts/tag-nodes.sh
 
@@ -24,7 +25,7 @@ helm repo add blakeblackshear https://blakeblackshear.github.io/blakeshome-chart
 echo -e "\n\nInstalling metallb"
 echo -e "=========================================================================================="
 helm upgrade metallb metallb/metallb \
-  --version $VERSION_METALLB \
+  --version $VERSION_METALLB_HELM \
   --values ./metallb/values.yaml \
   --namespace metallb-system \
   --create-namespace \
@@ -41,7 +42,7 @@ helm upgrade metallb-post ./metallb-post \
 echo -e "\n\nInstalling cert-manager"
 echo -e "=========================================================================================="
 helm upgrade cert-manager oci://quay.io/jetstack/charts/cert-manager \
-  --version $VERSION_CERT_MANAGER \
+  --version $VERSION_CERT_MGR_HELM \
   --set crds.enabled=true \
   --values ./cert-manager/values.yaml \
   --namespace cert-manager \
@@ -69,7 +70,7 @@ helm upgrade traefik-prep ./traefik-prep \
 echo -e "\n\nInstalling traefik"
 echo -e "=========================================================================================="
 helm upgrade traefik traefik/traefik \
-  --version $VERSION_TRAEFIK \
+  --version $VERSION_TRAEFIK_HELM \
   --values ./traefik/values.yaml \
   --namespace traefik \
   --create-namespace \
@@ -78,7 +79,7 @@ helm upgrade traefik traefik/traefik \
 echo -e "\n\nInstalling longhorn"
 echo -e "=========================================================================================="
 helm upgrade longhorn longhorn/longhorn \
-  --version $VERSION_LONGHORN \
+  --version $VERSION_LONGHORN_HELM \
   --values ./longhorn/values.yaml \
   --namespace longhorn-system \
   --create-namespace \
@@ -97,7 +98,7 @@ echo -e "\n\nInstalling technitium-dns"
 echo -e "=========================================================================================="
 helm upgrade technitium-dns ./technitium-dns \
   --values ./technitium-dns/values.yaml \
-  --set deployment.image.tag=$VERSION_TECHNITIUM \
+  --set deployment.image.tag=$VERSION_TECHNITIUM_IMG \
   --namespace technitium-dns \
   --create-namespace \
   --install
@@ -107,7 +108,7 @@ echo -e "=======================================================================
 helm upgrade go2rtc ./go2rtc \
   --values ./go2rtc/values.yaml \
   --values ./go2rtc/secrets.yaml \
-  --set deployment.image.tag=$VERSION_GO2RTC \
+  --set deployment.image.tag=$VERSION_GO2RTC_IMG \
   --namespace go2rtc \
   --create-namespace \
   --install
@@ -123,7 +124,7 @@ helm upgrade frigate-prep ./frigate-prep \
 echo -e "\n\nInstalling frigate"
 echo -e "=========================================================================================="
 helm upgrade frigate blakeblackshear/frigate \
-  --version $VERSION_FRIGATE \
+  --version $VERSION_FRIGATE_HELM \
   --values ./frigate/values.yaml \
   --set image.tag=$VERSION_FRIGATE_IMG \
   --namespace frigate \
@@ -136,5 +137,15 @@ echo -e "=======================================================================
 helm upgrade frigate-post ./frigate-post \
   --values ./frigate-post/values.yaml \
   --namespace frigate \
+  --create-namespace \
+  --install
+
+echo -e "\n\nInstalling tailscale"
+echo -e "=========================================================================================="
+helm upgrade tailscale ./tailscale \
+  --values ./tailscale/values.yaml \
+  --values ./tailscale/secrets.yaml \
+  --set deployment.image.tag=$VERSION_TAILSCALE_IMG \
+  --namespace tailscale \
   --create-namespace \
   --install
