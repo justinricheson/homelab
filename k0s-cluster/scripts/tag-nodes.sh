b7d4f750f575ef8bb6d517f2a68f664fa327faf3
Justin Richeson <justin@richeson.com>
2026-01-03 14:01:32 -0600

    Fix script comment

--- k0s-cluster/scripts/tag-nodes.sh
+++ k0s-cluster/scripts/tag-nodes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Taints nodes so deployments with hardware requirements go to the right place
+# Labels nodes so deployments with hardware requirements go to the right place
 
 set -euo pipefail
 
