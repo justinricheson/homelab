66aa93c55f498e2e48ad800bb43d7474e3731f87
Justin Richeson <justin@richeson.com>
2026-07-04 09:53:16 -0500

    Fix cron registration for busybox, hardcode tool paths, add cron logging

--- nas/rclone.sh
+++ nas/rclone.sh
@@ -2,10 +2,16 @@
 set -e
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
-CRON_JOB="0 2 * * * $DIR/rclone.sh run"
+CRON_JOB="0 2 * * * $DIR/rclone.sh >> $DIR/rclone.log 2>&1"
+if [ "$1" == "cron" ]; then
+  # Clear:   echo "" | sudo tee /usr/builtin/etc/crontabs/root
+  # Check:   cat /usr/builtin/etc/crontabs/root
+  # Restart: sudo /etc/init.d/S41crond restart
 
-if [ "$1" == "" ]; then
-  ( sudo crontab -l 2>/dev/null | grep -v "$(basename "$0")" ; echo "$CRON_JOB" ) | sudo crontab -
+  # Clear dupes
+  grep -v rclone.sh /usr/builtin/etc/crontabs/root 2>/dev/null | sudo tee /usr/builtin/etc/crontabs/root > /dev/null
+  echo "$CRON_JOB" | sudo tee -a /usr/builtin/etc/crontabs/root > /dev/null
+  sudo /etc/init.d/S41crond restart
   echo "Installed cron job: $CRON_JOB"
   exit 0
 fi
@@ -17,15 +23,8 @@ MQTT_TOPIC="nas/rclone/status"
 MQTT_USER="iot"
 #MQTT_PASS=Put in secrets.env
 
-RCLONE_SOURCE="/volume1/backup"
-RCLONE_REMOTE="remote:nas-backup-790055257995-us-east-1-an/backup"
-export RCLONE_CONFIG_REMOTE_TYPE=s3
-export RCLONE_CONFIG_REMOTE_REGION=us-east-1
-#export RCLONE_CONFIG_REMOTE_ACCESS_KEY_ID=Put in secrets.env
-#export RCLONE_CONFIG_REMOTE_SECRET_ACCESS_KEY=Put in secrets.env
-
 publish() {
-  mosquitto_pub -h "$MQTT_HOST" -p "$MQTT_PORT" --cafile "$CAFILE" \
+  /opt/bin/mosquitto_pub -h "$MQTT_HOST" -p "$MQTT_PORT" --cafile "$CAFILE" \
     -u "$MQTT_USER" -P "$MQTT_PASS" -t "$MQTT_TOPIC" -m "$1" -r
   echo "$1"
 }
@@ -36,8 +35,15 @@ if [ "$1" = "mqtt" ]; then
   exit 0
 fi
 
+export RCLONE_SOURCE="/volume1/backup"
+export RCLONE_REMOTE="remote:nas-backup-790055257995-us-east-1-an/backup"
+export RCLONE_CONFIG_REMOTE_TYPE=s3
+export RCLONE_CONFIG_REMOTE_REGION=us-east-1
+#export RCLONE_CONFIG_REMOTE_ACCESS_KEY_ID=Put in secrets.env
+#export RCLONE_CONFIG_REMOTE_SECRET_ACCESS_KEY=Put in secrets.env
+
 publish '{"status":"start","time":"'"$(date -u +%FT%TZ)"'"}'
-if rclone sync "$RCLONE_SOURCE" "$RCLONE_REMOTE" --log-level ERROR; then
+if /opt/bin/rclone sync "$RCLONE_SOURCE" "$RCLONE_REMOTE" --log-level ERROR; then
   publish '{"status":"success","time":"'"$(date -u +%FT%TZ)"'"}'
 else
   publish '{"status":"failure","time":"'"$(date -u +%FT%TZ)"'"}'
